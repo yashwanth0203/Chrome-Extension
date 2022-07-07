8248a77f42aaac2885245199fc5e9357456ac262
Yashwanth <mvyashwanthkumar@gmail.com>
2022-07-07 22:21:47 +0530

    Made anchor tags dynamic

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-let myLeads=[];
-const inputEl=document.getElementById("input-el");
-const inputBtn=document.getElementById("input-btn");
-const ulEl=document.getElementById("ul-el");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-inputBtn.addEventListener("click",function(){
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
+    inputEl.value = "";
     renderLeads();
     //console.log(myLeads);
 })
 
-function renderLeads(){
-let listItems="";
-for(let i=0;i<myLeads.length;i++){
-   listItems+="<li>"+myLeads[i]+"</li>";
-}
-ulEl.innerHTML=listItems;
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        //listItems+="<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</li>";
+    listItems +=
+            `<li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>`
+    }
+    ulEl.innerHTML = listItems;
 }
