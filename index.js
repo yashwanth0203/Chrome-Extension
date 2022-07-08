d227b23948dbc1e9a3f7f5b7817b7cac3b264427
Yashwanth <mvyashwanthkumar@gmail.com>
2022-07-08 16:50:51 +0530

    Added chrome API

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn=document.getElementById("delete-btn");
-
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn=document.getElementById("tab-btn");
+
 console.log(leadsFromLocalStorage)
 // 1. Check if leadsFromLocalStorage is truthy
 // 2. If so, set myLeads to its value and call renderLeads()
@@ -13,6 +14,18 @@ if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
     render(myLeads);
 }
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads);
+    });
+})
 
 function render(leads) {
     let listItems = "";
